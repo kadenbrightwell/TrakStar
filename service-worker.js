179de2fcc137fa0c536cdaeddc0719666b6ad043
Kaden Brightwell <61510196+kadenbrightwell@users.noreply.github.com>
2025-08-08 13:48:05 -0400

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,36 @@
+/* Simple offline + notification helper */
+const CACHE = "trakstar-v1";
+const ASSETS = [
+  "./",
+  "index.html",
+  "style.css",
+  "app.js",
+  "manifest.json",
+  "icon.png"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener("activate", e => {
+  e.waitUntil(self.clients.claim());
+});
+self.addEventListener("fetch", e => {
+  const req = e.request;
+  e.respondWith(
+    caches.match(req).then(res => res || fetch(req).then(r => {
+      const copy = r.clone();
+      caches.open(CACHE).then(c => c.put(req, copy)).catch(()=>{});
+      return r;
+    }).catch(()=>caches.match("./")))
+  );
+});
+
+// Notification shim: allow main page to call showNotification via SW
+self.addEventListener("message", evt => {
+  if (evt.data && evt.data.type === "showNotification") {
+    const { title, body, tag } = evt.data;
+    self.registration.showNotification(title, { body, tag, icon: "icon.png", badge: "icon.png" });
+  }
+});
